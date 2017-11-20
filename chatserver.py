5182514b04a800116656c14310c877ac9167eee2
Jobin Jose <jobinjose@outlook.com>
2017-11-20 18:58:54 +0000

    break

--- chatserver.py
+++ chatserver.py
@@ -203,7 +203,7 @@ class Client_Thread(Thread):
             elif "KILL_SERVICE" in msg_from_client:
                 print("got kill request")
                 tcp_socket.close()
-                sys.exit(0)
+                break
 
 
             elif "DISCONNECT" in msg_from_client:
